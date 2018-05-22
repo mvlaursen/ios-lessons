20f93256cafed1fcff901124a45113d4f75d862f
Mike Laursen <mlaursen@mikebook>
2018-05-22 13:16:10 -0700

    Some files were updated when I upgraded my laptop and Xcode.

--- SimpleTwo-X/SimpleTwoUITests/SimpleTwoUITests.swift
+++ SimpleTwo-X/SimpleTwoUITests/SimpleTwoUITests.swift
@@ -31,13 +31,6 @@ class SimpleTwoUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssert(true)
-    }
-    
-    func testExample2() {
-        let topTextFieldTextField = XCUIApplication().textFields["_top_text_field"]
-        topTextFieldTextField.tap()
-        topTextFieldTextField.typeText("hello")
     }
     
 }
