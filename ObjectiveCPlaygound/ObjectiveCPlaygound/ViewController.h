401ce4735fb69de35ea81cc4b211b024795bf8f3
Mike Laursen <mailbox@laursen.org>
2018-10-15 18:25:31 -0700

    Generated project

---
+++ ObjectiveCPlaygound/ObjectiveCPlaygound/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjectiveCPlaygound
+//
+//  Created by Mike Laursen on 10/15/18.
+//  Copyright © 2018 Appamajigger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
