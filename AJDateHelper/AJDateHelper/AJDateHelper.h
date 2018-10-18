7602f12b141842dad3645d45134b7721dd4a94c7
Mike Laursen <mailbox@laursen.org>
2018-10-18 14:05:55 -0700

    Generated framework

---
+++ AJDateHelper/AJDateHelper/AJDateHelper.h
@@ -0,0 +1,19 @@
+//
+//  AJDateHelper.h
+//  AJDateHelper
+//
+//  Created by Mike Laursen on 10/18/18.
+//  Copyright © 2018 Appamajigger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AJDateHelper.
+FOUNDATION_EXPORT double AJDateHelperVersionNumber;
+
+//! Project version string for AJDateHelper.
+FOUNDATION_EXPORT const unsigned char AJDateHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AJDateHelper/PublicHeader.h>
+
+
---
+++ AJDateHelper/AJDateHelperTests/AJDateHelperTests.m
@@ -0,0 +1,37 @@
+//
+//  AJDateHelperTests.m
+//  AJDateHelperTests
+//
+//  Created by Mike Laursen on 10/18/18.
+//  Copyright © 2018 Appamajigger. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AJDateHelperTests : XCTestCase
+
+@end
+
+@implementation AJDateHelperTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
