d1544a9d1d39c5577b8392b78a87373ef95dc186
Mike Laursen <mailbox@laursen.org>
2018-10-18 11:44:35 -0700

    Rough functionality

--- Tapper/Tapper/ViewController.m
+++ Tapper/Tapper/ViewController.m
@@ -26,6 +26,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    _HowManyTapsTxt.delegate = self;
     _isPlaying = NO;
     _currentTaps = 0;
     _maxTaps = 0;
@@ -33,12 +34,38 @@
 }
 
 - (IBAction)onCoinBtnTapped:(UIButton *)sender {
+    if (_maxTaps.intValue < 0)
+        _maxTaps = [NSNumber numberWithInteger:10];
+    
+    int intValue = _currentTaps.intValue;
+    intValue++;
+    _currentTaps = [NSNumber numberWithInteger:intValue];
+    
+    if (_currentTaps >= _maxTaps) {
+        _isPlaying = NO;
+        [self updateUI];
+    }
+}
+
+- (IBAction)onHowManyTapsDidEndOnExit:(UITextField *)sender {
+    // It would be better to use a Stepper control, but the assignment is to
+    // convert the Swift app as it was written.
+    
+    // If the contents of the text field cannot be converted to a double, zero
+    // is returned.
+    
+    double doubleValue = [sender.text doubleValue];
+    int intValue = floor(doubleValue);
+    _maxTaps = [NSNumber numberWithInteger:intValue];
+    _HowManyTapsTxt.text = [self formatMaxTaps];
 }
 
 - (IBAction)onPlayBtnTapped:(UIButton *)sender {
     _isPlaying = YES;
     [self updateUI];
 }
+
+#pragma mark Utility
 
 - (void)updateUI {
     if (_isPlaying) {
@@ -52,6 +79,22 @@
         _PlayBtn.enabled = YES;
         _PlayBtn.hidden = NO;
     }
+}
+
+- (NSString *)formatMaxTaps {
+    int intValue = _maxTaps.intValue;
+    
+    if (intValue > 0)
+        return [NSString stringWithFormat:@"%d", intValue];
+    else
+        return @"How many taps to win?";
+}
+
+#pragma mark UITextFieldDelegate
+
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
+    _HowManyTapsTxt.text = [self formatMaxTaps];
+    return YES;
 }
 
 @end
