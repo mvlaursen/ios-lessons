b11e3c93282a5435406d60aa8434c86aaa3e2dda
Mike Laursen <mailbox@laursen.org>
2018-10-18 10:21:14 -0700

    Switch between Play and Coin buttons.

--- Tapper/Tapper/ViewController.m
+++ Tapper/Tapper/ViewController.m
@@ -10,10 +10,11 @@
 
 @interface ViewController ()
 
-@property (weak, nonatomic) IBOutlet UIButton *coinBtn;
+@property (weak, nonatomic) IBOutlet UIButton *CoinBtn;
 @property (weak, nonatomic) IBOutlet UIButton *PlayBtn;
 @property (weak, nonatomic) IBOutlet UITextField *HowManyTapsTxt;
 
+@property (nonatomic) BOOL isPlaying;
 @property (strong, nonatomic) NSNumber *currentTaps;
 @property (strong, nonatomic) NSNumber *maxTaps;
 
@@ -25,15 +26,32 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    _isPlaying = NO;
     _currentTaps = 0;
     _maxTaps = 0;
+    [self updateUI];
 }
 
 - (IBAction)onCoinBtnTapped:(UIButton *)sender {
 }
 
 - (IBAction)onPlayBtnTapped:(UIButton *)sender {
-    
+    _isPlaying = YES;
+    [self updateUI];
+}
+
+- (void)updateUI {
+    if (_isPlaying) {
+        _CoinBtn.enabled = YES;
+        _CoinBtn.hidden = NO;
+        _PlayBtn.enabled = NO;
+        _PlayBtn.hidden = YES;
+    } else {
+        _CoinBtn.enabled = NO;
+        _CoinBtn.hidden = YES;
+        _PlayBtn.enabled = YES;
+        _PlayBtn.hidden = NO;
+    }
 }
 
 @end
