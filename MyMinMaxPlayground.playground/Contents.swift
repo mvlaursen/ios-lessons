12d6ff64f9a2aa806449c22acda5561457bf7fae
Mike Laursen <mailbox@laursen.org>
2018-11-06 11:48:27 -0800

    Make the description func recursive.

--- MyMinMaxPlayground.playground/Contents.swift
+++ MyMinMaxPlayground.playground/Contents.swift
@@ -10,6 +10,7 @@ enum Player {
 }
 
 class Node: CustomStringConvertible {
+    var depth: UInt = 0
     let player: Player
     let move: UInt
     var children: [Node] = []
@@ -20,13 +21,18 @@ class Node: CustomStringConvertible {
     }
     
     func addChild(_ node: Node) {
+        node.depth = self.depth + 1
         children.append(node)
     }
     
     var description: String {
         var description = "player: \(self.player) move: \(self.move) children: \(children.count)"
         for child in self.children {
-            description += "\n\tplayer: \(child.player) move: \(child.move)"
+            description += "\n"
+            for _ in 0..<child.depth {
+                description += "\t"
+            }
+            description += child.description
         }
         return description
     }
@@ -35,16 +41,16 @@ class Node: CustomStringConvertible {
 // A tree of alternating-turn moves to play around with...
 let b4 = Node(player: .black, move: 4)
     let b4_w2 = Node(player: .white, move: 2)
+    b4.addChild(b4_w2)
         let b4_w2_b8 = Node(player: .black, move: 8)
         b4_w2.addChild(b4_w2_b8)
         let b4_w2_b5 = Node(player: .black, move: 5)
         b4_w2.addChild(b4_w2_b5)
-    b4.addChild(b4_w2)
     let b4_w8 = Node(player: .white, move: 8)
+    b4.addChild(b4_w8)
         let b4_w8_b2 = Node(player: .black, move: 2)
         b4_w8.addChild(b4_w8_b2)
         let b4_w8_b5 = Node(player: .black, move: 2)
         b4_w8.addChild(b4_w8_b5)
-    b4.addChild(b4_w8)
 print(b4)
 
