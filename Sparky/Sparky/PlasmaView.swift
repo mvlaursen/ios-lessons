8f34d741871cccb47825a5b35ec30b6679c7b0b9
Mike Laursen <mailbox@laursen.org>
2018-11-23 17:12:55 -0800

    Implement all touch overrides.

--- Sparky/Sparky/PlasmaView.swift
+++ Sparky/Sparky/PlasmaView.swift
@@ -12,30 +12,12 @@ import UIKit
 class PlasmaView: UIView {
     static let colors = [UIColor.blue, UIColor.cyan, UIColor.green, UIColor.magenta, UIColor.purple, UIColor.red, UIColor.white]
     
-    var color: UIColor = UIColor.white
+    var color: UIColor = UIColor.black
     var touch: UITouch? = nil
     var touchPoint: CGPoint? = nil
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        assert(touches.count == 1)
-        if touches.count == 1 {
-            color = PlasmaView.colors.randomElement() ?? UIColor.white
-            touch = touches.first
-            touchPoint = touch?.location(in: self)
-            self.setNeedsDisplay()
-        }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let touchToTrack = touch {
-            if touches.contains(touchToTrack) {
-                print("----- Touch moved. -----")
-            }
-        }
     }
     
     // Only override draw() if you perform custom drawing.
@@ -48,6 +30,85 @@ class PlasmaView: UIView {
             path.move(to: self.center)
             path.addLine(to: mostRecentTouchPoint)
             path.stroke()
+        }
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        assert(touches.count == 1)
+        if touches.count == 1 {
+            color = PlasmaView.colors.randomElement() ?? UIColor.white
+            touch = touches.first
+            touchPoint = touch?.location(in: self)
+            self.setNeedsDisplay()
+        } else {
+            super.touchesBegan(touches, with: event)
+        }
+    }
+    
+    func stopTrackingTouch() {
+        color = UIColor.black
+        touch = nil
+        touchPoint = nil
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        var didHandleCancel = false
+        
+        if let touch = self.touch {
+            if touches.contains(touch) {
+                stopTrackingTouch()
+                didHandleCancel = true
+            }
+        }
+        
+        if !didHandleCancel {
+            super.touchesCancelled(touches, with: event)
+        }
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        var didHandleEnd = false
+        
+        if let touch = self.touch {
+            if touches.contains(touch) {
+                stopTrackingTouch()
+                didHandleEnd = true
+            }
+        }
+
+        if !didHandleEnd {
+            super.touchesEnded(touches, with: event)
+        }
+    }
+
+    override func touchesEstimatedPropertiesUpdated(_ touches: Set<UITouch>) {
+        var didHandleUpdate = false
+        
+        if let touch = self.touch {
+            if touches.contains(touch) {
+                touchPoint = touch.location(in: self)
+                didHandleUpdate = true
+            }
+        }
+        
+        if !didHandleUpdate {
+            super.touchesEstimatedPropertiesUpdated(touches)
+        }
+    }
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        var didHandleMove = false
+        
+        if let touch = self.touch {
+            if touches.contains(touch) {
+                touchPoint = touch.location(in: self)
+                self.setNeedsDisplay()
+                didHandleMove = true
+            }
+        }
+        
+        if !didHandleMove {
+            super.touchesMoved(touches, with: event)
         }
     }
 }
